8721a482fa0605129d48129156ce762e1d21744d
Ali Tahririan <66385997+amiralitah@users.noreply.github.com>
2025-10-06 13:11:23 -0500

    Add combined auth page and supporting assets

--- Model.py
+++ Model.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from datetime import datetime
 import uuid
-from .database import Base
+from database import Base
 
 
 class User(Base):
---
+++ models.py
@@ -0,0 +1,1 @@
+from Model import *  # noqa: F401,F403
